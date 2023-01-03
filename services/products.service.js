c7d6055b7891c3a07a784f09712d57fe8a88314a
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-01-03 17:59:33 +0100

    created service for adding product

--- services/products.service.js
+++ services/products.service.js
@@ -7,6 +7,27 @@ const getAllProducts = async (sort_by, order, filter) =>
     .sort({ [sort_by]: order === "desc" ? -1 : 1 })
     .toArray();
 
-// const addProduct = async();
+const checkIfExistsByName = async (name) =>
+  (await database.getCollection().findOne({ name: name })) !== null;
 
-module.exports = { getAllProducts };
+const addProduct = async (name, price, description, amount, unit) => {
+  try {
+    const ifUnique = await checkIfExistsByName(name);
+    if (ifUnique) {
+      const doc = {
+        name: name,
+        price: price,
+        description: description,
+        amount: amount,
+        unit: unit,
+      };
+      await database.getCollection().insertOne(doc);
+      return true;
+    }
+    return false;
+  } catch (e) {
+    return false;
+  }
+};
+
+module.exports = { getAllProducts, addProduct };
