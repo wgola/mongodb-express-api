b15b3ea6bbaceba85aa9938d4c5cbab9e228853a
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-01-04 14:12:08 +0100

    added editProduct service

--- services/products.service.js
+++ services/products.service.js
@@ -7,6 +7,9 @@ const getAllProducts = async (sort_by, order, filter) =>
     .sort({ [sort_by]: order === "desc" ? -1 : 1 })
     .toArray();
 
+const getProductById = async (id) =>
+  await database.getCollection().findOne({ _id: id });
+
 const checkIfExistsByName = async (name) =>
   (await database.getCollection().findOne({ name: name })) !== null;
 
@@ -26,6 +29,23 @@ const addProduct = async (name, price, description, amount, unit) => {
       return true;
     }
     return false;
+  } catch (e) {
+    return false;
+  }
+};
+
+const editProduct = async (id, name, price, description, amount, unit) => {
+  try {
+    const updated = {};
+    if (name) updated["name"] = name;
+    if (price) updated["price"] = price;
+    if (description) updated["description"] = description;
+    if (amount) updated["amount"] = amount;
+    if (unit) updated["unit"] = unit;
+    const result = await database
+      .getCollection()
+      .updateOne({ _id: id }, { $set: updated });
+    return result.acknowledged;
   } catch (e) {
     return false;
   }
