2851789a272ec80114c77e63afe4870fc1253be2
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-01-05 10:33:17 +0100

    refactored products services and db connection config

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    mongoose.set("strictQuery", false);
     mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to DB");
   } catch (e) {
--- services/products.service.js
+++ services/products.service.js
@@ -1,35 +1,22 @@
-const { ObjectId } = require("mongodb");
-const { database } = require("../db/dbConfig");
+const { products } = require("../models/product.model");
 
 const getAllProducts = async (sort_by, order, filter) =>
-  await database
-    .getCollection()
-    .find(filter)
-    .sort({ [sort_by]: order === "desc" ? -1 : 1 })
-    .toArray();
+  await products.find(filter).sort({ [sort_by]: order === "desc" ? -1 : 1 });
 
-const getProductById = async (id) =>
-  await database.getCollection().findOne({ _id: ObjectId(id) });
-
-const checkIfExistsByName = async (name) =>
-  (await database.getCollection().findOne({ name: name })) !== null;
+const getProductById = async (id) => await products.findById(id);
 
 const addProduct = async (name, price, description, amount, unit) => {
   try {
-    const ifUnique = !(await checkIfExistsByName(name));
-    if (ifUnique) {
-      const doc = {
-        name: name,
-        price: price,
-        description: description,
-        amount: amount,
-        unit: unit,
-        inOrderProcessing: false,
-      };
-      await database.getCollection().insertOne(doc);
-      return true;
-    }
-    return false;
+    const doc = {
+      name: name,
+      price: price,
+      description: description,
+      amount: amount,
+      unit: unit,
+      inOrderProcessing: false,
+    };
+    await products.create(doc);
+    return true;
   } catch (e) {
     return false;
   }
@@ -43,21 +30,19 @@ const editProduct = async (id, name, price, description, amount, unit) => {
     if (description) updated["description"] = description;
     if (amount) updated["amount"] = amount;
     if (unit) updated["unit"] = unit;
-    const result = await database
-      .getCollection()
-      .updateOne({ _id: ObjectId(id) }, { $set: updated });
+    const result = await products.updateOne({ _id: id }, updated);
     return result.acknowledged && result.modifiedCount === 1;
   } catch (e) {
-    console.log(e);
     return false;
   }
 };
 
 const deleteProductById = async (id) => {
   try {
-    const result = await database
-      .getCollection()
-      .deleteOne({ _id: ObjectId(id), inOrderProcessing: false });
+    const result = await products.deleteOne({
+      _id: id,
+      inOrderProcessing: false,
+    });
     return result.deletedCount === 1;
   } catch (e) {
     return false;
@@ -66,18 +51,16 @@ const deleteProductById = async (id) => {
 
 const createRaport = async () => {
   try {
-    const result = await database
-      .getCollection()
-      .aggregate([
-        {
-          $project: {
-            name: 1,
-            amount: { $concat: [{ $toString: "$amount" }, " ", "$unit"] },
-            totalPrice: { $multiply: ["$amount", "$price"] },
-          },
+    const result = await products.aggregate([
+      {
+        $project: {
+          name: 1,
+          amount: { $concat: [{ $toString: "$amount" }, " ", "$unit"] },
+          price: 1,
+          totalPrice: { $multiply: ["$amount", "$price"] },
         },
-      ])
-      .toArray();
+      },
+    ]);
     return result;
   } catch (e) {
     return [];
